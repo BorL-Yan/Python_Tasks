215f92fcbb1ff3589a193b6df98edbc455b7c2e2
Boris <137871704+BorL-Yan@users.noreply.github.com>
2025-04-30 08:05:13 +0000

    Task 2,dont finished

--- Task2.py
+++ Task2.py
@@ -10,61 +10,186 @@
 #   1. կոնստրուկտորներում մշակել բոլոր դաշտերի արժեքները
 #   2. սխալ արժեքների դեպքում մշակել համապատասխան Exception֊ներ
 
+class DateError(Exception):
+    pass
+
+class TimeError(Exception):
+    pass
+
 
 class Data:
-    def __init__(self):
-        pass
+
+    month_day = []
+    def __init__(self,day, month, year):
+        if not (0 <= year):
+            self._year = 0
+            raise DateError(f"The year should be positive : {year}. It has been assigned 0")
+        if not (1 <= month <= 12):
+            self._year = 0
+            raise DateError(f"The month sent is incorrect : {month}. It has been assigned 0")
+        if not (1 <= day <= self._days_in_month(year, month)):
+            self._day = 0
+            raise DateError(f"The day sent is incorrect : {day}. It has been assigned 0")
+        
+        self._day = day
+        self._month = month
+        self._year = year
+
+#region Private Metods
+    def _is_lerp_year(self, year):
+        return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+    
+    def _days_in_month(self, year, month):
+        days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        if month == 2  and self._is_lerp_year(year):
+            return 29
+        return days[month - 1]
+    
+    def _validate(self):
+        if not isinstance(self._year, int) or self._year < 0:
+            raise DateError(f"Year must be positive: {self._year}")
+        if not isinstance(self._month, int) or self._month < 1 or self._month > 12:
+            raise DateError(f"Month must be between 1 and 12: {self._month}")
+        if not isinstance(self._day, int) or self._day < 1 or self._day > self._days_in_month(self._year, self._month):
+            raise DateError(f"Day must be between 1 and {self._days_in_month(self._year, self._month)}: {self._day}")
+#end region
+
+    @property
+    def day(self):
+        return self._day
+
+    @day.setter
+    def day(self, value):
+        if not isinstance(value, int) or value < 0:
+            raise DateError(f"The day should be positive : {value}.")
+        
+        self._day = value
+        self._validate()
+    
+    def add_day(self, value):
+        if not isinstance(value, int) or value < 0:
+            raise DateError(f"The day should be positive : {value}.")
+        
+        while value > 0:
+            month_days = self._days_in_month(self.year, self.month)
+            if value > month_days:
+                self._day = 1
+                self.add_month(1)
+                value -= month_days
+            else:
+                
+                self._day = value
+                value = 0
+
+        self._validate()
+
+
+
+    @property
+    def month(self):
+        return self._month
+    
+    @month.setter
+    def month(self, value):
+        if not isinstance(value, int) or value < 0:
+            raise DateError(f"The month should be positive : {value}.")
+        
+        self._month = value
+        self._validate()
+
+    def add_month(self, value):
+        if not isinstance(value, int) or value < 0:
+            raise DateError(f"The month should be positive : {value}.")
+        
+        self.add_year( (value - 1) // 12 )
+        self._month = (value - 1) % 12 + 1
+
+    @property
+    def year(self):
+        return self._year
+    
+    @year.setter
+    def year(self, value):
+        if not isinstance(value, int) or value < 0:
+            raise DateError(f"The year should be positive : {value}.")
+        
+        self._year = value
+        self._validate()
+                
+
+    def add_year(self, value):
+        if not isinstance(value, int) or value < 0:
+            raise DateError(f"The year should be positive : {value}.")
+        
+        self._year += value
+        self._validate()
+
+
+  
+
+
 
 class Time:
     def __init__(self, date, second, minute, hour):
-        try:
-            self._date = date
-            self.append_hour(hour)
-            self.append_minute(minute)
-            self.append_second(second)
-        except Exception as e:
-            print (f" Exeption : {str(e)} ")
-            return -1
+    
+        self._date = date
 
-    def append_hour(self, hour):
-        if hour < 0 or hour > 24:
-            raise ValueError(f"invalidate value: {hour}")
-        self._hour = hour
-        return 0 
+        if not (0 <= hour <= 24):
+            self._hour = hour
+        else:
+            self._hour = 0
+            raise TimeError(f"The hour sent is incorrect : {hour}. It has been assigned 0")
+        
+        if not (0 <= minute <= 24):
+            self._minute = minute
+        else:
+            self._minute = 0
+            raise TimeError(f"The minute sent is incorrect : {minute}. It has been assigned 0")
+
+        if not (0 <= second <= 60):
+            self._second = second
+        else:
+            self._second = 0
+            raise TimeError(f"The second sent is incorrect : {second}. It has been assigned 0")
+
+    @property
+    def hour(self):
+        return self._hour
     
-    def append_hour(self, minute):
-        if minute < 0 or minute > 60:
-            raise ValueError(f"invalidate value: {minute}")
-        self._minute = minute
-        return 0 
-    
-    def append_hour(self, second):
-        if second < 0 or second > 60:
-            raise ValueError(f"invalidate value: {second}")
-        self._second = second
-        return 0 
-    
-    def add_hour(self, hour):
-        self._hour += hour
+    @hour.setter   
+    def hour(self,value):
+        self._hour += value
         day = int( self._hour / 24 )
         self._hour %= 24          
         self._date.add_day(day)
-        return 0
 
+    @property
+    def minute(self):
+        return self._minute
 
-    def add_minute(self, minute):
-        self._minute += minute
-        hour = int(self._minute / 24)
+    @minute.setter
+    def minute(self, value):
+        self._minute += value
+        hour = int(self._minute / 60)
         self._minute %= 60
-        self.add_hour(hour)
-        return 0
+        self.hour =+ hour
+    
 
+    @property
+    def second(self):
+        return self._second
 
-    def add_second(self, second):
-        self._second += second
-        minute = int(self._second / 24)
+    @second.setter
+    def second(self, value):
+        self._second += value
+        minute = int(self._second / 60)
         self._second %= 60
-        self.add_minute(minute)
-        return 0
+        self.minute += minute
 
+    def __str__(self):
+        return f"{self._hour}:{self._minute}:{self._second} {self._date.year}-{self._date.month}-{self._date.day}"
+
+
+time = Time(Data(2023, 10, 1), 0, 0, 0)
+print(time.hour)
 
